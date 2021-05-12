3368d070cef06a2cc84c42c86c8f559c584dad3d
MedyMa <75345465+MedyMa@users.noreply.github.com>
2021-05-12 15:57:25 +0800

    Update hook-feeds.sh

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -23,7 +23,7 @@ git clone --depth=1 https://github.com/immortalwrt/openwrt-tmate
 
 # Add luci-app-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall luci/applications/openwrt-passwall
-rm -rf luci/applications/openwrt-passwall/shadowsocksr-libev
+#rm -rf luci/applications/openwrt-passwall/shadowsocksr-libev
 
 # Add gotop
 svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/admin/gotop packages/admin/gotop
