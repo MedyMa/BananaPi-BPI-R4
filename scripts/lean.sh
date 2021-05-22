a30e9a18f410d6af7ff080a68d2b4b375a407a40
MedyMa <75345465+MedyMa@users.noreply.github.com>
2021-05-23 07:45:35 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -47,7 +47,11 @@ git clone --depth=1 https://github.com/NateLol/luci-app-oled
 # Add luci-app-adguardhome
 #svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome
 #svn co https://github.com/Lienol/openwrt/trunk/package/diy/adguardhome
- 
+
+# Add luci-app-smartdns & smartdns
+svn co https://github.com/281677160/openwrt-package/trunk/luci-app-smartdns
+svn co https://github.com/281677160/openwrt-package/trunk/smartdns
+
 # Add luci-app-diskman
 git clone --depth=1 https://github.com/SuLingGG/luci-app-diskman
 mkdir parted
