70685fcfb086901bb864cafd553b94431ea1d509
MedyMa <75345465+MedyMa@users.noreply.github.com>
2021-05-22 20:47:06 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -48,12 +48,6 @@ git clone --depth=1 https://github.com/NateLol/luci-app-oled
 #svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome
 #svn co https://github.com/Lienol/openwrt/trunk/package/diy/adguardhome
 
-# Add luci-app-netdata
-rm -rf ../lean/luci-app-netdata
-rm -rf ../../customfeeds/packages/admin/netdata
-svn co https://github.com/immortalwrt/luci/branches/openwrt-18.06/applications/luci-app-netdata
-svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/admin/netdata
-
 # Add luci-app-diskman
 git clone --depth=1 https://github.com/SuLingGG/luci-app-diskman
 mkdir parted
@@ -90,6 +84,12 @@ svn co https://github.com/zcy85611/Openwrt-Package/trunk/udpspeeder-tunnel
 # Add OpenAppFilter
 git clone --depth=1 https://github.com/destan19/OpenAppFilter
 popd
+
+# Add luci-app-netdata
+rm -rf package/lean/luci-app-netdata
+rm -rf customfeeds/packages/admin/netdata
+svn co https://github.com/immortalwrt/luci/branches/openwrt-18.06/applications/luci-app-netdata package/lean/luci-app-netdata
+svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/admin/netdata customfeeds/packages/admin/netdata
 
 # Add CPUInfo
 #pushd feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status
