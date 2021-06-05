7843d1f104b47b20db570f9eb30fef10edb86f74
MedyMa <75345465+MedyMa@users.noreply.github.com>
2021-06-05 10:15:17 +0800

    Update hook-feeds.sh

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -20,7 +20,7 @@ svn co https://github.com/immortalwrt/packages/trunk/net/gowebdav packages/net/g
 
 # Add luci-app-netdata
 rm -rf packages/admin/netdata
-svn co https://github.com/281677160/openwrt-package/trunk/feeds/packages/net/netdata packages/net/netdata
+svn co https://github.com/281677160/openwrt-package/trunk/feeds/packages/net/netdata packages/admin/netdata
 rm -rf ../package/lean/luci-app-netdata
 svn co https://github.com/281677160/openwrt-package/trunk/feeds/luci/applications/luci-app-netdata luci/applications/luci-app-netdata
 
