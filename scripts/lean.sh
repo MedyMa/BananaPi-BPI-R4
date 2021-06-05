7ace4608aeaa649f1b00010152e26cd728c20e92
MedyMa <75345465+MedyMa@users.noreply.github.com>
2021-06-05 10:40:45 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -48,12 +48,6 @@ git clone --depth=1 https://github.com/NateLol/luci-app-oled
 #svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome
 #svn co https://github.com/Lienol/openwrt/trunk/package/diy/adguardhome
 
-# Add luci-app-netdata
-rm -rf ../../customfeeds/packages/admin/netdata
-svn co https://github.com/281677160/openwrt-package/trunk/feeds/packages/net/netdata
-rm -rf ../lean/luci-app-netdata
-svn co https://github.com/281677160/openwrt-package/trunk/feeds/luci/applications/luci-app-netdata
-
 # Add luci-app-smartdns & smartdns
 svn co https://github.com/281677160/openwrt-package/trunk/feeds/luci/applications/luci-app-smartdns
 svn co https://github.com/281677160/openwrt-package/trunk/feeds/packages/net/smartdns
