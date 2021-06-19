d117499b6b2570346fc8e4abe8d61befcce54f68
MedyMa <75345465+MedyMa@users.noreply.github.com>
2021-06-19 19:56:37 +0800

    Update hook-feeds.sh

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -9,10 +9,8 @@
 pushd customfeeds
 
 # Add luci-app-adguardhome
-#svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-adguardhome luci/applications/luci-app-adguardhome
-#svn co https://github.com/immortalwrt/packages/trunk/net/adguardhome packages/net/adguardhome
-svn co https://github.com/281677160/openwrt-package/trunk/feeds/luci/applications/luci-app-adguardhome luci/applications/luci-app-adguardhome
-svn co https://github.com/281677160/openwrt-package/trunk/feeds/packages/net/adguardhome packages/net/adguardhome
+svn co https://github.com/immortalwrt/luci/branches/openwrt-18.06/applications/luci-app-adguardhome luci/applications/luci-app-adguardhome
+svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/net/adguardhome packages/net/adguardhome
 
 # Add luci-app-gowebdav
 svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-gowebdav luci/applications/luci-app-gowebdav
