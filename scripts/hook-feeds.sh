c4f9935e1e5fe2cc3cb0f6dbc831b9c343cc6066
MedyMa <75345465+MedyMa@users.noreply.github.com>
2021-08-31 09:01:57 +0800

    Update hook-feeds.sh

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -8,6 +8,17 @@
 # Svn checkout packages from immortalwrt's repository
 pushd customfeeds
 
+# Add luci-app-cpufreq
+svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-cpufreq luci/applications/luci-app-cpufreq
+rm -rf ../package/lean/luci-app-cpufreq
+
+# Add luci-app-cpulimit
+svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-cpulimit luci/applications/luci-app-cpulimit
+svn co https://github.com/immortalwrt/packages/trunk/utils/cpulimit packages/cpulimit
+
+# Add luci-app-eqos
+svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-eqos luci/applications/luci-app-eqos
+
 # Add luci-app-gowebdav
 svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-gowebdav luci/applications/luci-app-gowebdav
 svn co https://github.com/immortalwrt/packages/trunk/net/gowebdav packages/net/gowebdav
