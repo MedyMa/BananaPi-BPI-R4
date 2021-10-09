2f478124c3ecba19871be83ec7532aa76c7d5ab0
MedyMa <75345465+MedyMa@users.noreply.github.com>
2021-10-10 06:43:56 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -17,6 +17,8 @@ pushd package/community
 
 # Add Lienol's Packages
 git clone --depth=1 https://github.com/Lienol/openwrt-package
+rm -rf openwrt-package/verysync
+rm -rf openwrt-package/luci-app-verysync
 
 # Add luci-app-passwall
 #git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
