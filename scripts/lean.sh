8998d66ca3681c732190bfa6e9ef8da57a536f44
MedyMa <75345465+MedyMa@users.noreply.github.com>
2022-03-24 20:06:01 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -14,7 +14,7 @@ popd
 #Delete
 pushd package/kernel/linux/modules
 rm -rf fs.mk
-svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-21.02/package/kernel/linux/modules/fs.mk
+svn co https://github.com/immortalwrt/immortalwrt/branches/master/package/kernel/linux/modules/fs.mk
 popd
 
 # Clone community packages to package/community
