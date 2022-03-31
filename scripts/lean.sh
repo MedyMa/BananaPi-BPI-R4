2df7bbd27086f9e9cfe711cd10e595e22afaeb07
MedyMa <75345465+MedyMa@users.noreply.github.com>
2022-03-31 17:51:44 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -11,6 +11,12 @@ pushd package/lean
 git clone --depth=1 https://github.com/fw876/helloworld
 popd
 
+#Delete
+pushd package/kernel/linux/modules
+rm -rf fs.mk
+svn co https://github.com/DHDAXCW/wget/trunk/fs.mk
+popd
+
 # Clone community packages to package/community
 mkdir package/community
 pushd package/community
