7c6da0d5b6a9c90f4aefdb9ee7f89891f55bb38f
MedyMa <75345465+MedyMa@users.noreply.github.com>
2022-05-23 10:46:22 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -6,6 +6,11 @@
 # Blog: https://p3terx.com
 #=================================================
 
+# Add luci-app-ssr-plus
+pushd package/lean
+git clone --depth=1 https://github.com/fw876/helloworld
+popd
+
 # Clone community packages to package/community
 mkdir package/community
 pushd package/community
@@ -15,9 +20,6 @@ git clone --depth=1 https://github.com/Lienol/openwrt-package
 rm -rf ../../customfeeds/luci/applications/luci-app-kodexplorer
 rm -rf openwrt-package/verysync
 rm -rf openwrt-package/luci-app-verysync
-
-# Add luci-app-ssr-plus
-git clone --depth=1 https://github.com/fw876/helloworld
 
 # Add luci-app-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
