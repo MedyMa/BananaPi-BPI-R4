bb88e8bddbae1e1d20037c5f63d8e0a80265e245
MedyMa <75345465+MedyMa@users.noreply.github.com>
2022-07-19 08:18:24 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -145,7 +145,7 @@ sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # Test kernel 5.10
-#sed -i 's/5.10/5.15/g' target/linux/rockchip/Makefile
+sed -i 's/5.18/5.15/g' target/linux/rockchip/Makefile
 
 # Custom configs
 
