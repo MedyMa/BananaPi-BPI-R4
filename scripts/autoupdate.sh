9306c8b6a633f13e6cda5f2dc32ab8d8ec19514b
MedyMa <75345465+MedyMa@users.noreply.github.com>
2023-01-09 20:37:10 +0800

    Update autoupdate.sh

--- scripts/autoupdate.sh
+++ scripts/autoupdate.sh
@@ -5,7 +5,7 @@ opkg install gzip
 cd /tmp
 rm -rf artifact openwrt-rockchip*.img.gz openwrt-rockchip*img*
 echo -e '\e[92m准备下载升级文件\e[0m'
-wget https://github.com/MedyMa/NanoPi-R4S/releases/download/$(date +%Y.%m.%d)-Lean/openwrt-rockchip-armv8-friendlyarm_nanopi-r4s-ext4-sysupgrade.img.gz
+wget https://github.com/MedyMa/NanoPi-R4S/releases/download/$(date +%Y.%m.%d)-Lean/openwrt-rockchip-armv8-friendlyarm_nanopi-r4s-squashfs-sysupgrade.img.gz
 if [ -f /tmp/openwrt-rockchip-armv8-friendlyarm_nanopi-r4s-ext4-sysupgrade.img.gz	]; then
 	echo -e '\e[92m今天固件已下载，准备解压\e[0m'
 else
