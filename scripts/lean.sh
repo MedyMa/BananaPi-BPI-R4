d642c4e9f8ca50898f90995186fe14928bc1b25d
MedyMa <75345465+MedyMa@users.noreply.github.com>
2023-09-14 09:37:17 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -21,7 +21,7 @@ git clone --depth=1 https://github.com/fw876/helloworld
 
 # Add luci-app-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
-svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages
 
 # Add luci-app-vssr <M>
 git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb.git
