c9bdb4c41e82f43ad8d3852e329028de2da62e17
MedyMa <75345465+MedyMa@users.noreply.github.com>
2024-01-26 10:28:50 +0800

    Update hook-feeds.sh

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -6,24 +6,6 @@
 #=================================================
 # Svn checkout packages from immortalwrt's repository
 
-pushd customfeeds
-
-# Add luci-app-eqos
-svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-eqos luci/applications/luci-app-eqos
-
-# Add luci-proto-modemmanager
-svn co https://github.com/immortalwrt/luci/trunk/protocols/luci-proto-modemmanager luci/protocols/luci-proto-modemmanager
-
-# Add tmate
-git clone --depth=1 https://github.com/immortalwrt/openwrt-tmate
-
-# Add gotop
-svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/admin/gotop packages/admin/gotop
-
-# Add minieap
-svn co https://github.com/immortalwrt/packages/trunk/net/minieap packages/net/minieap
-popd
-
 # Set to local feeds
 pushd customfeeds/packages
 export packages_feed="$(pwd)"
