1a6cad36fd35142c3c04d598535010c78fb428ac
MedyMa <75345465+MedyMa@users.noreply.github.com>
2024-01-26 14:03:08 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -10,6 +10,20 @@
 mkdir package/community
 pushd package/community
 
+# Merge_package
+function merge_package(){
+    repo=`echo $1 | rev | cut -d'/' -f 1 | rev`
+    pkg=`echo $2 | rev | cut -d'/' -f 1 | rev`
+    # find package/ -follow -name $pkg -not -path "package/openwrt-packages/*" | xargs -rt rm -rf
+    git clone --depth=1 --single-branch $1
+    [ -d package/openwrt-packages ] || mkdir -p package/openwrt-packages
+    mv $2 package/openwrt-packages/
+    rm -rf $repo
+}
+rm -rf ../../customfeeds/packages/utils/apk
+# Add apk (Apk Packages Manager)
+merge_package https://github.com/openwrt/packages utils/apk
+
 # Add Lienol's Packages
 git clone --depth=1 https://github.com/Lienol/openwrt-package
 rm -rf ../../customfeeds/luci/applications/luci-app-kodexplorer
