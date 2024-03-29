0027297e9fe656a877e1e4089f673be7dcb4b17f
MedyMa <75345465+MedyMa@users.noreply.github.com>
2024-03-29 20:38:41 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -24,8 +24,6 @@ function merge_package(){
 rm -rf ../../customfeeds/packages/utils/apk
 # Add apk (Apk Packages Manager)
 merge_package https://github.com/openwrt/packages packages/utils/apk
-merge_package https://github.com/immortalwrt/luci luci/applications/luci-app-alist
-merge_package https://github.com/immortalwrt/packages packages/net/alist
 
 # Add Lienol's Packages
 git clone --depth=1 https://github.com/Lienol/openwrt-package
