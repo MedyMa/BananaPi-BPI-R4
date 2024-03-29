23c58719e6e91a8c9c613120083c3e1da38ccbdb
MedyMa <75345465+MedyMa@users.noreply.github.com>
2024-03-29 15:56:45 +0800

    Update hook-feeds.sh

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -8,15 +8,20 @@
 
 # Set to local feeds
 pushd customfeeds/packages
-export packages_feed="$(pwd)"
+git clone --depth=1 https://github.com/linkease/nas-packages
+rm -rf nas-packages/multimedia
+rm -rf nas-packages/network/services/{linkease,quickstart,unishare,webdav2}
 rm -rf net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
+export packages_feed="$(pwd)"
+popd
+pushd customfeeds/luci
 git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb.git
 git clone --depth=1 https://github.com/MilesPoupart/luci-app-vssr
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages
 git clone --depth=1 https://github.com/fw876/helloworld
-popd
-pushd customfeeds/luci
+git clone --depth=1 https://github.com/linkease/nas-packages-luci
+rm -rf nas-packages-luci/luci/{luci-app-istorex,luci-app-linkease,luci-app-quickstart,luci-app-unishare,luci-lib-iform}
 export luci_feed="$(pwd)"
 popd
 sed -i '/src-git packages/d' feeds.conf.default
