c25a6f40a9720987eece1ec8f338a521abbf2d6e
MedyMa <75345465+MedyMa@users.noreply.github.com>
2024-07-12 13:48:13 +0800

    Update hook-feeds.sh

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -25,6 +25,8 @@ git clone --depth=1 https://github.com/linkease/nas-packages-luci
 # git clone --depth=1 https://github.com/sbwml/luci-app-alist
 git clone --depth=1 https://github.com/MedyMa/luci-app-adguardhome
 rm -rf nas-packages-luci/luci/{luci-app-istorex,luci-app-linkease,luci-app-quickstart,luci-app-unishare,luci-lib-iform}
+git clone --depth=1 https://github.com/jjm2473/openwrt-apps.git
+rm -rf openwrt-apps/{homebox,luci-app-cpufreg,luci-app-homeboX,luci-app-tasks,luci-lib-mac-vendor,LICENSE,README.md,luci-alias.mk}
 export luci_feed="$(pwd)"
 popd
 sed -i '/src-git packages/d' feeds.conf.default
