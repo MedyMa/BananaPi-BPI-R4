7862f9733adec519c41d9422087d9a5cc9c8303a
MedyMa <75345465+MedyMa@users.noreply.github.com>
2024-07-16 09:17:26 +0800

    Update hook-feeds.sh

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -29,16 +29,10 @@ git clone --depth=1 https://github.com/jjm2473/openwrt-apps.git
 rm -rf openwrt-apps/{homebox,luci-app-cpufreg,luci-app-homeboX,luci-app-tasks,luci-lib-mac-vendor,LICENSE,README.md,luci-alias.mk}
 export luci_feed="$(pwd)"
 popd
-sed -i "/helloworld/d" "feeds.conf.default"
-echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
-./scripts/feeds update helloworld
-./scripts/feeds install -a -f -p helloworld
 sed -i '/src-git packages/d' feeds.conf.default
 echo "src-link packages $packages_feed" >> feeds.conf.default
 sed -i '/src-git luci/d' feeds.conf.default
 echo "src-link luci $luci_feed" >> feeds.conf.default
 
 # Update feeds
-./scripts/feeds update helloworld
-./scripts/feeds install -a -f -p helloworld
 ./scripts/feeds update -a
