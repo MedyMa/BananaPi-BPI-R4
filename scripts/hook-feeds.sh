fc1fe46017db5c140f8d5f36ac6110775f257ec2
MedyMa <75345465+MedyMa@users.noreply.github.com>
2024-07-17 09:26:10 +0800

    Update hook-feeds.sh

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -19,7 +19,9 @@ function merge_package(){
 
 # Set to local feeds
 rm -rf package/wwan/*
+pushd package/wwan
 merge_package https://github.com/DHDAXCW/lede-rockchip package/wwan
+popd
 pushd customfeeds/packages
 rm -rf net/adguardhome
 git clone --depth=1 https://github.com/linkease/nas-packages
