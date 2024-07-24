8101f210372b7df3bea3aa134ccf8693bba16a2f
MedyMa <75345465+MedyMa@users.noreply.github.com>
2024-07-24 09:30:31 +0800

    Update hook-feeds.sh

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -42,10 +42,10 @@
 # # merge_package https://github.com/DHDAXCW/lede-rockchip lede-rockchip/package/wwan
 # export luci_feed="$(pwd)"
 # popd
-sed -i '/src-git packages/d' feeds.conf.default
-echo "src-link packages $packages_feed" >> feeds.conf.default
-sed -i '/src-git luci/d' feeds.conf.default
-echo "src-link luci $luci_feed" >> feeds.conf.default
+# sed -i '/src-git packages/d' feeds.conf.default
+# echo "src-link packages $packages_feed" >> feeds.conf.default
+# sed -i '/src-git luci/d' feeds.conf.default
+# echo "src-link luci $luci_feed" >> feeds.conf.default
 
 # Update feeds
 ./scripts/feeds update -a
