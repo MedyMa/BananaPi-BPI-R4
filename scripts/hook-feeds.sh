d88df17bc7f0414c937957002644dede8eb59bb9
MedyMa <75345465+MedyMa@users.noreply.github.com>
2024-07-29 16:52:28 +0800

    Update hook-feeds.sh

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -10,7 +10,7 @@
 function merge_package(){
     repo=`echo $1 | rev | cut -d'/' -f 1 | rev`
     pkg=`echo $2 | rev | cut -d'/' -f 1 | rev`
-    # find package/ -follow -name $pkg -not -path "package/openwrt-packages/*" | xargs -rt rm -rf
+    find package/ -follow -name $pkg -not -path "package/openwrt-packages/*" | xargs -rt rm -rf
     git clone --depth=1 --single-branch $1
     [ -d package/openwrt-packages ] || mkdir -p package/openwrt-packages
     mv $2 package/openwrt-packages/
