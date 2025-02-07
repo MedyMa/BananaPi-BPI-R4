4f71050de91c70c169d51ea3458f65f20f907872
MedyMa <75345465+MedyMa@users.noreply.github.com>
2025-02-07 19:14:42 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -58,16 +58,16 @@ echo -e "OpenWrt built on "$(date +%Y.%m.%d)"\n --------------------------------
 #cp -f $GITHUB_WORKSPACE/scripts/cpuinfo cpuinfo
 #popd
 
-# Add Wifi7 Drivers
-pushd package/kernel/mt76/patches
-cp -f $GITHUB_WORKSPACE/patches/lean/0058-mtk-mt76-mt7996-add-Eagle-2adie-TBTC-BE14000-support.patch 0058-mtk-mt76-mt7996-add-Eagle-2adie-TBTC-BE14000-support.patch
-popd
-pushd package/kernel/mt76
-rm -rf Makefile
-cp -f $GITHUB_WORKSPACE/patches/lean/Makefile Makefile
-popd
-mkdir files
-mkdir files/lib/firmware/mediatek/mt7996
-pushd files/lib/firmware/mediatek/mt7996
-cp -f $GITHUB_WORKSPACE/patches/mt7966/*
-popd
+# # Add Wifi7 Drivers
+# pushd package/kernel/mt76/patches
+# cp -f $GITHUB_WORKSPACE/patches/lean/0058-mtk-mt76-mt7996-add-Eagle-2adie-TBTC-BE14000-support.patch 0058-mtk-mt76-mt7996-add-Eagle-2adie-TBTC-BE14000-support.patch
+# popd
+# pushd package/kernel/mt76
+# rm -rf Makefile
+# cp -f $GITHUB_WORKSPACE/patches/lean/Makefile Makefile
+# popd
+# mkdir files
+# mkdir files/lib/firmware/mediatek/mt7996
+# pushd files/lib/firmware/mediatek/mt7996
+# cp -f $GITHUB_WORKSPACE/patches/mt7966/*
+# popd
