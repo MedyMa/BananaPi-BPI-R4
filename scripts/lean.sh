0c688eda2a561f83d1517f20b07957351aac6ae0
MedyMa <75345465+MedyMa@users.noreply.github.com>
2025-02-28 22:16:37 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -43,6 +43,10 @@ popd
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
+# Test
+sed -i 's/kmod-mt7996-firmware/kmod-mt7996-233-firmware/g' target/linux/mediatek/image/filogic.mk
+
+
 # Test kernel 5.10
 #sed -i 's/6.1/6.6/g' target/linux/rockchip/Makefile
 
