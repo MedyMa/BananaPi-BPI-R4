c77395d1e418ea074cc3ae91e86f078ffc1263a8
MedyMa <75345465+MedyMa@users.noreply.github.com>
2025-03-10 17:02:41 +0800

    Update diy-part1.sh

--- immortalwrt/diy-part1.sh
+++ immortalwrt/diy-part1.sh
@@ -3,7 +3,7 @@
 # Add a feed source
 #echo 'src-git moruiris https://github.com/moruiris/openwrt-packages;immortalwrt' >>feeds.conf.default
 git clone -b Immortalwrt https://github.com/shidahuilang/openwrt-package ./package/openwrt-packages
-
+rm -rf ./package/openwrt-packages/relevance/alist
 # Clone community packages to package/community
 mkdir package/community
 pushd package/community
