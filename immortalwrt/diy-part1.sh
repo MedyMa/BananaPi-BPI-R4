35cdaf3acfff34d0e20ad76b853866ef9c975edf
MedyMa <75345465+MedyMa@users.noreply.github.com>
2025-03-10 14:09:24 +0800

    Update diy-part1.sh

--- immortalwrt/diy-part1.sh
+++ immortalwrt/diy-part1.sh
@@ -3,7 +3,12 @@
 # Add a feed source
 #echo 'src-git moruiris https://github.com/moruiris/openwrt-packages;immortalwrt' >>feeds.conf.default
 git clone -b Immortalwrt https://github.com/shidahuilang/openwrt-package ./package/openwrt-packages
-git clone --depth=1 https://github.com/fw876/helloworld ./package/openwrt-packages
+
+# Clone community packages to package/community
+mkdir package/community
+pushd package/community
+git clone --depth=1 https://github.com/fw876/helloworld
 # Add luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon ./package/openwrt-packages
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config ./package/openwrt-packages
+git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
+popd
