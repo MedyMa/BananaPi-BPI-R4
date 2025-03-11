b56d12d0619c826b65460f38045010dd3bf73a65
MedyMa <75345465+MedyMa@users.noreply.github.com>
2025-03-11 10:35:48 +0800

    Update diy-part1.sh

--- immortalwrt/diy-part1.sh
+++ immortalwrt/diy-part1.sh
@@ -2,16 +2,7 @@
 #
 # Add a feed source
 #echo 'src-git moruiris https://github.com/moruiris/openwrt-packages;immortalwrt' >>feeds.conf.default
-# Merge_package
-function merge_package(){
-    repo=`echo $1 | rev | cut -d'/' -f 1 | rev`
-    pkg=`echo $2 | rev | cut -d'/' -f 1 | rev`
-    # find package/ -follow -name $pkg -not -path "package/openwrt-packages/*" | xargs -rt rm -rf
-    git clone --depth=1 --single-branch $1
-    [ -d package/openwrt-packages ] || mkdir -p package/openwrt-packages
-    mv $2 package/openwrt-packages/
-    rm -rf $repo
-}
+
 rm -rf packege/base-files
 merge_package "-b openwrt-24.10 https://github.com/openwrt/openwrt/package/base-files" package/base-files
 git clone -b Immortalwrt https://github.com/shidahuilang/openwrt-package ./package/openwrt-packages
@@ -22,6 +13,18 @@ rm -rf ./package/openwrt-packages/luci-app-clouddrive2
 rm -rf ./package/openwrt-packages/luci-app-floatip
 rm -rf ./package/openwrt-packages/luci-app-nginx-pingos
 rm -rf ./package/openwrt-packages/luci-app-syncthing
+
+# Merge_package
+function merge_package(){
+    repo=`echo $1 | rev | cut -d'/' -f 1 | rev`
+    pkg=`echo $2 | rev | cut -d'/' -f 1 | rev`
+    # find package/ -follow -name $pkg -not -path "package/openwrt-packages/*" | xargs -rt rm -rf
+    git clone --depth=1 --single-branch $1
+    [ -d package/openwrt-packages ] || mkdir -p package/openwrt-packages
+    mv $2 package/openwrt-packages/
+    rm -rf $repo
+}
+
 # Clone community packages to package/community
 mkdir package/community
 pushd package/community
