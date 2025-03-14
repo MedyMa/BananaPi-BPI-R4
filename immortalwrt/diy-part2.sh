046859de0b651a924249adaba53fceeaf072c277
MedyMa <75345465+MedyMa@users.noreply.github.com>
2025-03-14 14:37:37 +0800

    Update diy-part2.sh

--- immortalwrt/diy-part2.sh
+++ immortalwrt/diy-part2.sh
@@ -2,4 +2,4 @@
 #
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-
+sed -i "s/ImmortalWrt/OpenWrt/g" package/base-files/files/bin/config_generate
