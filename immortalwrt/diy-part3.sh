4bcd1f58947210569487cf2f647e0387997133e6
MedyMa <75345465+MedyMa@users.noreply.github.com>
2025-06-30 15:20:28 +0800

    Update diy-part3.sh

--- immortalwrt/diy-part3.sh
+++ immortalwrt/diy-part3.sh
@@ -51,5 +51,3 @@ rm -rf package/firmware/wireless-regdb/patches/*.*
 rm -rf package/firmware/wireless-regdb/Makefile
 cp -f $GITHUB_WORKSPACE/patches/filogic/500-tx_power.patch package/firmware/wireless-regdb/patches
 cp -f $GITHUB_WORKSPACE/patches/filogic/regdb.Makefile package/firmware/wireless-regdb/Makefile
-rm -rf package/kernel/r8152
-merge_package "-b openwrt-24.10 https://github.com/immortalwrt/immortalwrt" immortalwrt/package/kernel/r8152
