c444792818dc09869cf025118c4b8ca4c0d0bf7a
MedyMa <75345465+MedyMa@users.noreply.github.com>
2025-07-03 09:41:02 +0800

    Update diy-part3.sh

--- immortalwrt/diy-part3.sh
+++ immortalwrt/diy-part3.sh
@@ -27,7 +27,6 @@ git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
 merge_package https://github.com/kenzok8/jell jell/luci-app-serverchan
 merge_package "-b Immortalwrt https://github.com/shidahuilang/openwrt-package" openwrt-package/relevance/ddnsto
 merge_package "-b Immortalwrt https://github.com/shidahuilang/openwrt-package" openwrt-package/luci-app-ddnsto
-merge_package "-b Immortalwrt https://github.com/shidahuilang/openwrt-package" openwrt-package/luci-app-ddnsto
 popd
 
 # add luci-app-mosdns
