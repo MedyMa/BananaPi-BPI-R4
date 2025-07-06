921b823751e15ab27b9e3722284c4de61f0a09c3
MedyMa <75345465+MedyMa@users.noreply.github.com>
2025-07-06 12:42:09 +0800

    Update diy-part3.sh

--- immortalwrt/diy-part3.sh
+++ immortalwrt/diy-part3.sh
@@ -49,3 +49,5 @@ rm -rf package/firmware/wireless-regdb/patches/*.*
 rm -rf package/firmware/wireless-regdb/Makefile
 cp -f $GITHUB_WORKSPACE/patches/filogic/500-tx_power.patch package/firmware/wireless-regdb/patches
 cp -f $GITHUB_WORKSPACE/patches/filogic/regdb.Makefile package/firmware/wireless-regdb/Makefile
+rm -rf target/linux/mediatek/files-6.6/arch/arm64/boot/dts/mediatek/mt7988a.dtsi
+cp -f $GITHUB_WORKSPACE/patches/filogic/mt7988a.dtsi target/linux/mediatek/files-6.6/arch/arm64/boot/dts/mediatek/mt7988a.dtsi
