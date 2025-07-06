33426e0cc81704b64da1784b61ea4b90f3445283
MedyMa <75345465+MedyMa@users.noreply.github.com>
2025-07-06 19:16:03 +0800

    Update diy-part1.sh

--- immortalwrt/diy-part1.sh
+++ immortalwrt/diy-part1.sh
@@ -52,5 +52,5 @@ cp -f $GITHUB_WORKSPACE/patches/filogic/500-tx_power.patch package/firmware/wire
 cp -f $GITHUB_WORKSPACE/patches/filogic/regdb.Makefile package/firmware/wireless-regdb/Makefile
 merge_package https://github.com/DHDAXCW/lede-rockchip lede-rockchip/package/wwan
 # merge_package "-b mt7988-2410-6.6 https://github.com/padavanonly/immortalwrt-mt798x-6.6" immortalwrt-mt798x-6.6/package/mtk
-merge_package "-b mt7988-2410-6.6 https://github.com/padavanonly/immortalwrt-mt798x-6.6" immortalwrt-mt798x-6.6/target/linux/mediatek/files-6.6/drivers/net/ethernet/mediatek
-cp -f $GITHUB_WORKSPACE/patches/filogic/999-2745-mtkhnat-add-mtkhnat-driver-support.patch target/linux/mediatek/patches-6.6/999-2745-mtkhnat-add-mtkhnat-driver-support.patch
+# merge_package "-b mt7988-2410-6.6 https://github.com/padavanonly/immortalwrt-mt798x-6.6" immortalwrt-mt798x-6.6/target/linux/mediatek/files-6.6/drivers/net/ethernet/mediatek
+# cp -f $GITHUB_WORKSPACE/patches/filogic/999-2745-mtkhnat-add-mtkhnat-driver-support.patch target/linux/mediatek/patches-6.6/999-2745-mtkhnat-add-mtkhnat-driver-support.patch
