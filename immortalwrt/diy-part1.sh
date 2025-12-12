be531a1ce5cef27df6f6e6ae15714fdbccdd4476
MedyMa <75345465+MedyMa@users.noreply.github.com>
2025-12-12 16:26:30 +0800

    Update diy-part1.sh

--- immortalwrt/diy-part1.sh
+++ immortalwrt/diy-part1.sh
@@ -21,7 +21,7 @@ git clone --depth=1 https://github.com/fw876/helloworld
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages
 git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki
-git clone --depth=1 https://github.com/DHDAXCW/dhdaxcw-app
+git clone --depth=1 https://github.com/sirpdboy/luci-app-adguardhome
 git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
 # merge_package https://github.com/kenzok8/jell jell/luci-app-fan
