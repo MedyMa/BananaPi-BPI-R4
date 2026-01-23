1ba10c0456734c05680cef73e6a22308063383e9
MedyMa <75345465+MedyMa@users.noreply.github.com>
2026-01-23 09:05:18 +0800

    Update diy-part3.sh

--- immortalwrt/diy-part3.sh
+++ immortalwrt/diy-part3.sh
@@ -30,7 +30,7 @@ popd
 
 # add luci-app-mosdns
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
 rm -rf feeds/packages/net/v2ray-geodata
 rm -rf feeds/packages/net/mosdns
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
