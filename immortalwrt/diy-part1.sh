e97554ff3700268cc619ed744b3e1750766b18f7
MedyMa <75345465+MedyMa@users.noreply.github.com>
2026-03-28 16:41:41 +0800

    Update golang package branch to 25.x

--- immortalwrt/diy-part1.sh
+++ immortalwrt/diy-part1.sh
@@ -35,7 +35,7 @@ popd
 
 # add luci-app-mosdns
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
 rm -rf feeds/packages/net/mosdns
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 
