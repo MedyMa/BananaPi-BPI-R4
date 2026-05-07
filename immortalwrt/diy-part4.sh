98d6b765e929516ce8284aec54cfb3122836d1f5
MedyMa <75345465+MedyMa@users.noreply.github.com>
2026-05-07 16:20:28 +0800

    Change default IP and comment out fortify workaround
    Updated default IP address from 192.168.1.1 to 192.168.2.1 in config_generate. Commented out the GCC fortify workaround code for mbedtls.

--- immortalwrt/diy-part4.sh
+++ immortalwrt/diy-part4.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Modify default IP
-sed -i 's/192.168.6.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # Workaround: GCC 14 + musl fortify "always_inline memset: target specific option mismatch" in mbedtls
 # Root cause: When building for aarch64_cortex-a53 with GCC 14, TARGET_CFLAGS includes
@@ -10,10 +10,10 @@ sed -i 's/192.168.6.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 # target-option consistency for always_inline functions and raises an error.
 # Fix: Disable _FORTIFY_SOURCE only for mbedtls so the fortify inline is not attempted,
 # resolving the mismatch without affecting any other package's compilation.
-if ! grep -q '_FORTIFY_SOURCE=0' package/libs/mbedtls/Makefile; then
-  if grep -q 'TARGET_CFLAGS := \$(filter-out -O%' package/libs/mbedtls/Makefile; then
-    sed -i '/TARGET_CFLAGS := \$(filter-out -O%/a TARGET_CFLAGS += -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=0' package/libs/mbedtls/Makefile
-  else
-    echo 'TARGET_CFLAGS += -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=0' >> package/libs/mbedtls/Makefile
-  fi
-fi
+# if ! grep -q '_FORTIFY_SOURCE=0' package/libs/mbedtls/Makefile; then
+#   if grep -q 'TARGET_CFLAGS := \$(filter-out -O%' package/libs/mbedtls/Makefile; then
+#     sed -i '/TARGET_CFLAGS := \$(filter-out -O%/a TARGET_CFLAGS += -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=0' package/libs/mbedtls/Makefile
+#   else
+#     echo 'TARGET_CFLAGS += -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=0' >> package/libs/mbedtls/Makefile
+#   fi
+# fi
