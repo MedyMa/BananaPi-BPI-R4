d67fab93c3d12fb26a451fa1408c1de7116b7759
MedyMa <75345465+MedyMa@users.noreply.github.com>
2026-05-07 08:47:19 +0800

    Clean up feeds by removing unused applications
    Removed specific applications and packages from feeds.

--- immortalwrt/diy-part1.sh
+++ immortalwrt/diy-part1.sh
@@ -16,7 +16,6 @@ rm -rf feeds/luci/applications/luci-app-argon-config
 rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 
-
 # Clone community packages to package/community
 mkdir package/community
 pushd package/community
