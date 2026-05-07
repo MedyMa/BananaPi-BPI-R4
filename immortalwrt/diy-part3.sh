1da3c7d89bc5fa30bc46d29c61b2dac87c159236
MedyMa <75345465+MedyMa@users.noreply.github.com>
2026-05-07 16:19:54 +0800

    Update package management and dependencies in script

--- immortalwrt/diy-part3.sh
+++ immortalwrt/diy-part3.sh
@@ -13,32 +13,35 @@ function merge_package(){
 
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-argon-config
+rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 
 # Clone community packages to package/community
 mkdir package/community
 pushd package/community
 git clone --depth=1 https://github.com/fw876/helloworld
-git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall
-git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall-packages
+git clone --depth=1 -b main https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git
+git clone --depth=1 -b main https://github.com/Openwrt-Passwall/openwrt-passwall.git
 git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki
 git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
-git clone --depth=1 https://github.com/Siriling/5G-Modem-Support
+# git clone --depth=1 https://github.com/Siriling/5G-Modem-Support
 # merge_package https://github.com/kenzok8/jell jell/luci-app-fan
+merge_package https://github.com/Siriling/5G-Modem-Support 5G-Modem-Support/luci-app-modem
 merge_package https://github.com/DHDAXCW/dhdaxcw-app dhdaxcw-app/luci-app-adguardhome
+merge_package https://github.com/MedyMa/luci-app-sfp-status luci-app-sfp-status/luci-app-sfp-status
 merge_package https://github.com/kenzok8/jell jell/wrtbwmon
+# merge_package "-b Immortalwrt https://github.com/shidahuilang/openwrt-package" openwrt-package/relevance/ddnsto
+# merge_package "-b Immortalwrt https://github.com/shidahuilang/openwrt-package" openwrt-package/luci-app-ddnsto
 merge_package "-b ddnsto-beta https://github.com/linkease/nas-packages-luci" nas-packages-luci/luci/luci-app-ddnsto
 merge_package "-b ddnsto-beta https://github.com/linkease/nas-packages" nas-packages/network/services/ddnsto
-sed -i 's/^PKG_SOURCE_DATE:=.*/PKG_SOURCE_DATE:=4.0.7/' package/openwrt-packages/ddnsto/Makefile
-sed -i 's/^PKG_HASH:=.*/PKG_HASH:=425cdb809f06e805e481e772e168309df44c591205be7f72f347f61c4200b42b/' package/openwrt-packages/ddnsto/Makefile
 popd
 
 # add luci-app-mosdns
-rm -rf feeds/packages/net/mosdns
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
+rm -rf feeds/packages/net/mosdns
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 # add luci-app-OpenClash
 mkdir package/OpenClash
@@ -48,16 +51,10 @@ git config core.sparsecheckout true
 popd
 
 # wireless-regdb modification
-rm -rf package/firmware/wireless-regdb/patches/*.*
-rm -rf package/firmware/wireless-regdb/Makefile
-cp -f $GITHUB_WORKSPACE/patches/filogic/500-tx_power.patch package/firmware/wireless-regdb/patches
-cp -f $GITHUB_WORKSPACE/patches/filogic/regdb.Makefile package/firmware/wireless-regdb/Makefile
-rm -rf package/network/config/wifi-scripts
-rm -rf package/firmware/wireless-regdb
-pushd  package/network/config
-merge_package "-b openwrt-24.10 https://github.com/immortalwrt/immortalwrt" immortalwrt/package/network/config/wifi-scripts
-popd
-pushd  package/firmware
-merge_package "-b openwrt-24.10 https://github.com/immortalwrt/immortalwrt" immortalwrt/package/firmware/wireless-regdb
-popd
-# cp -f $GITHUB_WORKSPACE/patches/filogic/lvts_enable.patch target/linux/mediatek/patches-6.6/lvts_enable.patch
+# rm -rf package/firmware/wireless-regdb/patches/*.*
+# rm -rf package/firmware/wireless-regdb/Makefile
+# cp -f $GITHUB_WORKSPACE/patches/filogic/500-tx_power.patch package/firmware/wireless-regdb/patches/500-tx_power.patch
+# cp -f $GITHUB_WORKSPACE/patches/filogic/regdb.Makefile package/firmware/wireless-regdb/Makefile
+merge_package "-b openwrt-24.10-6.6 https://github.com/padavanonly/immortalwrt-mt798x-6.6" immortalwrt-mt798x-6.6/package/mtk/applications/luci-app-Airpifanctrl
+merge_package "-b openwrt-24.10-6.6 https://github.com/padavanonly/immortalwrt-mt798x-6.6" immortalwrt-mt798x-6.6/package/mtk/applications/Airpi-gpio-fan
+./scripts/feeds update -a
