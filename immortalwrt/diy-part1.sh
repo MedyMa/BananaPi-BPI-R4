60485e9a2d0f753ddc703d4d3ef9e817ad064dd4
MedyMa <75345465+MedyMa@users.noreply.github.com>
2026-05-14 15:42:19 +0800

    Remove modemband app and update merges in diy-part1.sh

--- immortalwrt/diy-part1.sh
+++ immortalwrt/diy-part1.sh
@@ -24,6 +24,7 @@ patch_makefile_dep() {
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-argon-config
 rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-modemband
 rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 
 # Clone community packages to package/community
@@ -43,6 +44,7 @@ git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
 merge_package https://github.com/MedyMa/luci-app-sfp-status luci-app-sfp-status/Luci-app/luci-app-fan
 merge_package https://github.com/MedyMa/luci-app-sfp-status luci-app-sfp-status/Luci-app/luci-app-sfp-status
 merge_package https://github.com/MedyMa/luci-app-sfp-status luci-app-sfp-status/Luci-app/luci-app-adguardhome
+merge_package https://github.com/MedyMa/luci-app-sfp-status luci-app-sfp-status/Luci-app/luci-app-modemband
 merge_package https://github.com/kenzok8/jell jell/wrtbwmon
 # merge_package "-b Immortalwrt https://github.com/shidahuilang/openwrt-package" openwrt-package/relevance/ddnsto
 # merge_package "-b Immortalwrt https://github.com/shidahuilang/openwrt-package" openwrt-package/luci-app-ddnsto
