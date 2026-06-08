d39483612b9df50f66147d4dacf03866979a3868
MedyMa <75345465+MedyMa@users.noreply.github.com>
2026-06-08 08:41:55 +0800

    Update diy-part5.sh

--- immortalwrt/diy-part5.sh
+++ immortalwrt/diy-part5.sh
@@ -55,7 +55,7 @@ rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2sock
 # Clone community packages to package/community
 mkdir -p package/community
 pushd package/community
-git clone --depth=1 https://github.com/fw876/helloworld
+git clone --depth=1 -b dev https://github.com/fw876/helloworld
 # rm -rf helloworld/{naiveproxy,shadowsocks-libev,shadowsocksr-libev,shadow-tls,simple-obfs,tcping,tuic-client,v2ray-plugin,xray-core,xray-plugin}
 git clone --depth=1 -b main https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git
 [ -f openwrt-passwall-packages/haproxy/Makefile ] && sed -i '/^[[:space:]]*ADDON+=USE_QUIC=1$/d' openwrt-passwall-packages/haproxy/Makefile
