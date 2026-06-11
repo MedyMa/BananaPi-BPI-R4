1e999f468014be9e5b41de0ed12016b48d6c60ef
MedyMa <75345465+MedyMa@users.noreply.github.com>
2026-06-11 22:20:38 +0800

    Update diy-part5.sh

--- immortalwrt/diy-part5.sh
+++ immortalwrt/diy-part5.sh
@@ -71,8 +71,8 @@ merge_package https://github.com/MedyMa/luci-app luci-app/Luci-app/luci-app-sfp-
 merge_package https://github.com/MedyMa/luci-app luci-app/Luci-app/luci-app-adguardhome
 merge_package https://github.com/MedyMa/luci-app luci-app/Luci-app/luci-app-modemband
 merge_package https://github.com/kenzok8/jell jell/wrtbwmon
-merge_package "-b main https://github.com/linkease/nas-packages-luci" nas-packages-luci/luci/luci-app-ddnsto
-merge_package "-b master https://github.com/linkease/nas-packages" nas-packages/network/services/ddnsto
+merge_package "-b main https://github.com/linkease/ddnsto-openwrt-package" ddnsto-openwrt-package/ddnsto
+merge_package "-b main https://github.com/linkease/ddnsto-openwrt-package" ddnsto-openwrt-package/luci-app-ddnsto
 popd
 
 # Wireless stack migration lives in diy-part6.sh to keep diy-part5 focused on
